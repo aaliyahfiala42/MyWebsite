d3fda3cf867463774b1a02edcd617d6aa1312f04
aaliyahfiala42 <aaliyahfiala42@gmail.com>
2021-04-24 18:28:32 -0700

    Added more content

--- app.py
+++ app.py
@@ -7,30 +7,29 @@ app = Flask(__name__)
 
 # Create the route for homepage upload.html
 @app.route('/')
-def upload_form():
+def home_page():
     return render_template('home.html')
 
-# # Create the route for the about me page
-# @app.route('/about', methods=['GET', 'POST'])
-# def about_page():
-#     return render_template('about.html')
-#
-#
-# # Create the route for the portfolio page
-# @app.route('/portfolio', methods=['GET', 'POST'])
-# def portfolio_page():
-#     return render_template('portfolio.html')
-#
-#
-# # Create the route for resume page
-# @app.route('/resume', methods=['GET', 'POST'])
-# def resume_page():
-#     return render_template('resume.html')
-#
-# # Create the route for the Contact Me page
-# @app.route('/contact', methods=['GET', 'POST'])
-# def contactpage():
-#     return render_template('contact.html')
+# Create the route for the about me page
+@app.route('/home')
+def home_page_return():
+    return render_template('home.html')
+
+# Create the route for the portfolio page
+@app.route('/portfolio')
+def portfolio_page():
+    return render_template('portfolio.html')
+
+
+# Create the route for resume page
+@app.route('/resume')
+def resume_page():
+    return render_template('resume.html')
+
+# Create the route for the Contact Me page
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
 
 
 
